930912ff0bc72e6eea16dffb76985a9a4d8107fe
UragaMarin <uraga.marin.merid.italibi@gmail.com>
2024-01-15 14:48:03 -0600

    si

---
+++ src/app/navbar-a/navbar-a.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-a',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar-a.component.html',
+  styleUrl: './navbar-a.component.css'
+})
+export class NavbarAComponent {
+
+}
