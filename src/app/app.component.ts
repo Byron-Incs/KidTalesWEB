0b6dcba24a7a7a5e4258c8250a8c937836d83879
UragaMarin <uraga.marin.merid.italibi@gmail.com>
2024-01-15 15:02:48 -0600

    Cambio en navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarBComponent } from './navbar-b/navbar-b.component';
+import { NavbarAComponent } from './navbar-a/navbar-a.component';
+import { FooterAComponent } from './footer-a/footer-a.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarAComponent, FooterAComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
