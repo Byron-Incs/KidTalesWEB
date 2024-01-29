5300697e3a6db1d63dc53e0354db1761eef212fe
UragaMarin <uraga.marin.merid.italibi@gmail.com>
2024-01-29 15:07:31 -0600

    Por favor, no falles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,27 +1,30 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
-import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyByRMv40h5tVDQkI5vwtQLT1DfWWvCnOAc",
+  authDomain: "kid-tales1.firebaseapp.com",
+  projectId: "kid-tales1",
+  storageBucket: "kid-tales1.appspot.com",
+  messagingSenderId: "539360258544",
+  appId: "1:539360258544:web:573fbcc8bca47dd32a1535",
+  measurementId: "G-76C435K39W"
+};
+
+initializeApp(firebaseConfig);
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes), 
-    provideClientHydration(),
-    importProvidersFrom([
-      provideFirebaseApp(() => initializeApp({
-        apiKey: "AIzaSyByRMv40h5tVDQkI5vwtQLT1DfWWvCnOAc",
-        authDomain: "kid-tales1.firebaseapp.com",
-        projectId: "kid-tales1",
-        storageBucket: "kid-tales1.appspot.com",
-        messagingSenderId: "539360258544",
-        appId: "1:539360258544:web:573fbcc8bca47dd32a1535",
-        measurementId: "G-76C435K39W"
-      })),
-      provideFirestore(() => getFirestore()),
-    ])
-  ]
+  providers: [provideRouter(routes),    
+  importProvidersFrom(
+    HttpClientModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
+  ) ]
 };
