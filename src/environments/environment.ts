9218eaa5adab091286c6824256e4c2a4f27d4015
UragaMarin <uraga.marin.merid.italibi@gmail.com>
2024-01-31 15:52:50 -0600

    Esperoq eu sean buenas prácticas

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyByRMv40h5tVDQkI5vwtQLT1DfWWvCnOAc",
+    authDomain: "kid-tales1.firebaseapp.com",
+    projectId: "kid-tales1",
+    storageBucket: "kid-tales1.appspot.com",
+    messagingSenderId: "539360258544",
+    appId: "1:539360258544:web:573fbcc8bca47dd32a1535",
+    measurementId: "G-76C435K39W"
+  },
+};
