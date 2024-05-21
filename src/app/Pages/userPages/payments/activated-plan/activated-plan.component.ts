15ebc7abb70b4953bb90ebca3eb6eaba247e92d4
UragaMarin <uraga.marin.merid.italibi@gmail.com>
2024-05-20 20:49:20 -0600

    Actualización en Mi Subscripción

--- src/app/Pages/userPages/payments/activated-plan/activated-plan.component.ts
+++ src/app/Pages/userPages/payments/activated-plan/activated-plan.component.ts
@@ -17,6 +17,7 @@ import { GooglePayButtonModule } from '@google-pay/button-angular';
   templateUrl: './activated-plan.component.html',
   styleUrl: './activated-plan.component.css'
 })
+
 export class ActivatedPlanComponent {
   private activatedRoute = inject(ActivatedRoute);
   private readonly userService = inject(UserService);
@@ -27,5 +28,57 @@ export class ActivatedPlanComponent {
     const id = this.activatedRoute.snapshot.params['id'];
     this.userId! = this.activatedRoute.snapshot.params['id'];
     this.user$ = this.userService.getUser(id);
+  }
+
+  buttonWidth = 240;
+
+  paymentRequest: google.payments.api.PaymentDataRequest = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [
+      {
+        type: 'CARD',
+        parameters: {
+          allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+          allowedCardNetworks: ["AMEX", "DISCOVER", "INTERAC", "JCB", "MASTERCARD", "VISA"]
+        },
+        tokenizationSpecification: {
+          type: 'PAYMENT_GATEWAY',
+          parameters: {
+            gateway: 'example',
+            gatewayMerchantId: 'exampleGatewayMerchantId'
+          }
+        }
+      }
+    ],
+    merchantInfo: {
+      merchantId: '12345678901234567890',
+      merchantName: 'Demo Merchant'
+    },
+    transactionInfo: {
+      totalPriceStatus: 'FINAL',
+      totalPriceLabel: 'Total',
+      totalPrice: '100.00',
+      currencyCode: 'USD',
+      countryCode: 'US'
+    }
+  }
+
+  onLoadPaymentData(event: any) {
+    console.log(event, ">> Data");
+    console.log('Payment successful:', event.detail);
+
+    const fechaISO = new Date();
+    const fechaISOString = fechaISO.toISOString();
+    const partesFecha = fechaISOString.split('T');
+    const fechaModificada = partesFecha[0] + ' ' + partesFecha[1].replace('Z', '');
+
+    this.userService.updateUserPlan(this.userId, true, fechaModificada)
+      .then(() => {
+        console.log('User plan updated successfully');
+      })
+      .catch(error => {
+        console.error('Error updating user plan:', error);
+      });
   }
 }
